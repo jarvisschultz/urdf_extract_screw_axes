e3f7f15f2587653483acc378820d86e53b1b2b05
Jarvis Schultz <schultzjarvis@gmail.com>
2017-02-20 12:30:36 -0600

    Initial versions of setup and README

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+install_requires = [
+    'numpy',
+    'urdf_parser_py'
+    ]
+
+test_requires = [
+    'modern_robotics'
+    ]
+
+
+setup(
+    name='urdf_extract_screw_axes',
+    version='0.1dev',
+    author=['Jarvis Schultz'],
+    author_email='schultzjarvis@gmail.com',
+    packages=['urdf_extract_screw_axes',],
+    description='Tool for converting URDF robot descriptions to screw axes',
+    long_description=open('README.org').read(),
+    install_requires=install_requires,
+    test_requires=test_requires
+)
